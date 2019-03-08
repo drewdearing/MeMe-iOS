57d39ec9b74590e62289cc0408e247f842f3670e
Yair Nieto <yair.nieto.cofo2015@gmail.com>
2019-03-08 02:06:16 -0600

    Alpa, making UI VC

---
+++ MeMe/MeMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  MeMe
+//
+//  Created by Yair Sanchez Nieto on 3/7/19.
+//  Copyright © 2019 meme. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
