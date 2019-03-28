47706888c04d8794aa1d020256eb0bd31c44cf82
drewdearing <social@drewdearing.com>
2019-03-27 21:27:41 -0500

    upload segue sync

--- MeMe/MeMe/Controllers/EditMemeViewController.swift
+++ MeMe/MeMe/Controllers/EditMemeViewController.swift
@@ -32,35 +32,15 @@ class EditMemeViewController: UIViewController {
     
     
     @IBAction func postMeme(_ sender: Any) {
-        if let currentUser = Auth.auth().currentUser {
-            self.addToStorage()
-            /*
-            var ref: DocumentReference? = nil
-            ref = Firestore.firestore().collection("post").addDocument(data: [
-                "bottomText": "Meme",
-                "description": descriptionField.text!,
-                "downvotes": 0,
-                "photoURL": photoURL,
-                "timestamp": NSDate(),
-                "topText": "Dank",
-                "uid": currentUser.uid,
-                "upvotes": 0
-                ]) { err in
-                if let err = err {
-                    print("Error adding document: \(err)")
-                } else {
-                    print("Document added with ID: \(ref!.documentID)")
-                }
-            }
-            */
-        }
+        addToStorage()
     }
     
     func addToStorage(){
         let storage = Storage.storage()
         let storageRef = storage.reference()
         let data = selectedImage.pngData()
-        let imageName = NSUUID().uuidString
+        let ref = Firestore.firestore().collection("post").document()
+        let imageName = ref.documentID
         print(imageName)
         let imageRef = storageRef.child("\(imageName).png")
         
@@ -80,8 +60,7 @@ class EditMemeViewController: UIViewController {
                 if let urlText = url?.absoluteString {
                     let currentUser = Auth.auth().currentUser
                     let download = urlText
-                    var ref: DocumentReference? = nil
-                    ref = Firestore.firestore().collection("post").addDocument(data: [
+                    ref.setData([
                         "bottomText": "Meme",
                         "description": self.descriptionField.text!,
                         "downvotes": 0,
@@ -94,7 +73,8 @@ class EditMemeViewController: UIViewController {
                         if let err = err {
                             print("Error adding document: \(err)")
                         } else {
-                            print("Document added with ID: \(ref!.documentID)")
+                            print("Document added with ID: \(ref.documentID)")
+                            self.performSegue(withIdentifier: "FinishEditMemeSegue", sender: self)
                         }
                     }
                 }
@@ -105,25 +85,5 @@ class EditMemeViewController: UIViewController {
             }
         }
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if (segue.identifier == "DoneViewVCIdentifier"){
-            //we're going to the username VC
-            postMeme(self)
-            let mainVC: MainViewController = (segue.destination as! MainViewController)
-        }
-        
-    }
-
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
