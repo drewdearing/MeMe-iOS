de43f9f0fd1923f8ef0ee8072b937ed24e635031
drewdearing <social@drewdearing.com>
2019-04-10 15:40:34 -0500

    Use ImagePicker Instead of GalleryVC

--- MeMe/MeMe/Controllers/DiscoverViewController.swift
+++ MeMe/MeMe/Controllers/DiscoverViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
-class DiscoverViewController: TabViewController, NewMemeDelegate, PostNavigationDelegate {
+class DiscoverViewController: TabViewController, NewMemeDelegate, PostNavigationDelegate, ImagePickerDelegate {
+    
     @IBOutlet weak var feedView: DiscoverView!
+    var imagePicker:ImagePicker!
+    var selectedImage:UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         feedView.delegate = self
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
     }
     
     func addMeme(post: FeedCellData, feed:Bool) {
@@ -24,10 +28,19 @@ class DiscoverViewController: TabViewController, NewMemeDelegate, PostNavigation
         navigationController?.pushViewController(postVC, animated: true)
     }
     
+    @IBAction func selectImage(_ sender: Any) {
+        self.imagePicker.present(from: self.view)
+    }
+    
+    func didSelect(image: UIImage?) {
+        selectedImage = image
+        performSegue(withIdentifier: "NewMemeSegue", sender: self)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "NewMemeSegue" {
-            let destinationNavigationController = segue.destination as! UINavigationController
-            let dest = destinationNavigationController.topViewController as! GalleryViewController
+            let dest = segue.destination as! EditMemeViewController
+            dest.selectedImage = selectedImage
             dest.delegate = self
         }
     }
